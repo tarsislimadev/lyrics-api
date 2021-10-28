1ef336fba2465b88efee22e66edc7e38510fc81c
Tarsis de Lima <br.tmvdl@gmail.com>
2021-10-27 22:19:56 -0300

    Fixing creation of artist directory

--- app/index.mjs
+++ app/index.mjs
@@ -10,17 +10,18 @@ const songTitleURL = 'https://api.lyrics.ovh/v1/'
 // CONSTANTS
 const artist = 'Mamonas Assassinas'
 
+// HELPERS
+const fixString = (str = '') => str.replace(/ /ig, '+')
+
+const pathOfArtist = (artist) => path.resolve('.', 'songs', fixString(artist))
+
 // FUNCTIONS
 const findArtist = async (artist) => await fetch(artistURL + fixString(artist)).then(res => res.json())
 
 const findLyrics = (artist, songTitle) => fetch(songTitleURL + fixString(artist) + '/' + fixString(songTitle))
 	.then((res) => res.json())
-	.then(({ lyrics = '' }) => fs.writeFileSync(path.resolve('.', 'songs', artist, songTitle + '.md'), lyrics))
+	.then(({ lyrics = '' }) => fs.writeFileSync(path.resolve(pathOfArtist(artist), fixString(songTitle) + '.md'), lyrics))
 	.catch(err => console.error(artist, songTitle, err))
 
-// HELPERS
-const fixString = (str) => str.replace(' ', '+')
-
 // RUN
-fs.mkdir(path.resolve('.', 'songs', artist), { recursive: true }, () => findArtist(artist).then(({ data }) => Promise.all(data.map(({ title }) => findLyrics(artist, title)))))
-
+fs.mkdir(pathOfArtist(artist), { recursive: true }, () => findArtist(artist).then(({ data }) => Promise.all(data.map(({ title }) => findLyrics(artist, title)))))
